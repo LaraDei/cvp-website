0b67ba0e31ee727c0ee8fbaeb798869d44f863ec
Katie <25576005+stuttskl@users.noreply.github.com>
2021-04-25 10:26:01 -0700

    added about details component and info

--- src/Components/AboutDetails.jsx
+++ src/Components/AboutDetails.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import imgPlaceholder from '../Assets/img.jpg'
 import './AboutDetails.css'
 
-const AboutDetails = () => {
+const AboutDetails = (props) => {
   return (
     <div class="container-sm">
-      <img src={imgPlaceholder} class="about-img"/>
-      <h6>First Last</h6>
-      <p>
-        description
-      </p>
+      <img src={imgPlaceholder} class="about-img"/> 
+      <div class="container" id="about-info">
+        <h5>{props.name}</h5>
+        <h6>{props.title}</h6>
+        <p>
+          {props.description}
+        </p>
+      </div>
     </div>
   );
 };
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './About.css'
 import logo from '../Assets/CVP.png'
 import AboutDetails from '../Components/AboutDetails'
+import data from '../data/team.json'
 
 const About = () => {
   return (
@@ -13,50 +14,19 @@ const About = () => {
       <section class="container-fluid">
         <h1 class="display-6">Our Team</h1>
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-        </div>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-        </div>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-          <div class="col">
-            <AboutDetails />
-          </div>
-        </div>
-      </section>
-
-   
+          {data.map((person, id) =>
+          (
+            <div class="col">
+              <AboutDetails 
+                name={person.name}
+                title={person.title}
+                img={person.img}
+                description={person.description}
+              />
+            </div>
+          ))}
+      </div>
+    </section>
   </div>
   );
 };
