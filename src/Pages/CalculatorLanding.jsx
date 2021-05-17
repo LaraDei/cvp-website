9cbd22c196c090911432bacb8d18b84429258231
Janu <janu.m.sung@gmail.com>
2021-05-17 16:50:41 -0700

    modified json structure

--- src/Pages/CalculatorLanding.jsx
+++ src/Pages/CalculatorLanding.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Button from "../Components/Button";
 //! Import css
 
@@ -7,26 +7,26 @@ import Button from "../Components/Button";
  * Will always show welcome message, description and a start button
  */
 const CalculatorLanding = () => {
-	/** Use history hook to start the questionaire  */
-	const history = useHistory();
+  /** Use history hook to start the questionaire  */
+  const history = useHistory();
 
-	/** Callback for Start button to begin the questionaire */
-	const onStartClick = () => {
-		/** User clicks start */
-		/** User is sent to first question */
-		history.push("/calculator/0");
-	}
+  /** Callback for Start button to begin the questionaire */
+  const onStartClick = () => {
+    /** User clicks start */
+    /** User is sent to first question */
+    history.push("/calculator/0");
+  };
 
-	return (
-		<div>
-			<h1>Welcome to Conviction Vacation Project!</h1>
-			{/* // TODO Import the figma text */}
-			<p>Grab the text from Figma.</p>
-		
-			{/* When a user clicks this they will begin the questionaire */}
-			<Button text="Start" onClick={onStartClick} />
-		</div>
-	)
-}
+  return (
+    <div>
+      <h1>Welcome to Conviction Vacation Project!</h1>
+      {/* // TODO Import the figma text */}
+      <p>Grab the text from Figma.</p>
+
+      {/* When a user clicks this they will begin the questionaire */}
+      <Button text="Start" onClick={onStartClick} />
+    </div>
+  );
+};
 
 export default CalculatorLanding;
--- src/Pages/CalculatorQuestion.jsx
+++ src/Pages/CalculatorQuestion.jsx
@@ -1,32 +1,36 @@
 import React from "react";
-import Button from "../Components/Button"
-import {useParams} from "react-router-dom";
+import Button from "../Components/Button";
+import { useParams } from "react-router-dom";
 
 // data import
 import Questions from "../data/calcuatorQuestions.json";
 
 /** A page to display an individual question from the question list */
 const CalculatorQuestion = () => {
-	/** Grab the number that comes after /calculator in the url */
-	const { number } = useParams();
+  /** Grab the number that comes after /calculator in the url */
+  const { number } = useParams();
 
-	/** 
-	 * Filter through Questions file to find question to display
-	 * useParams returns a string, so we have to parseInt the `number` value
-	 * Filter returns an array so we grab the first element 
-	 * and destructure the question properties: question, tooltip, and answers
-	 */
-	const {question, tooltip, answers} = Questions.filter(q => q.id === parseInt(number))[0];
+  /**
+   * Filter through Questions file to find question to display
+   * useParams returns a string, so we have to parseInt the `number` value
+   * Filter returns an array so we grab the first element
+   * and destructure the question properties: question, tooltip, and answers
+   */
+  const { question, tooltip, answers } = Questions.filter(
+    (q) => q.id === parseInt(number)
+  )[0];
 
-	return (
-		<div>
-			<h1>{question}</h1>
-			{tooltip}
-			<ul>
-				{answers.map(a => <Button text={a} onClick={() => console.log(a)}/>)}
-			</ul>
-		</div>
-	)
-}
+  return (
+    <div>
+      <h1>{question}</h1>
+      {tooltip}
+      <ul>
+        {answers.map((a) => (
+          <Button text={a} onClick={() => console.log(a)} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default CalculatorQuestion;
