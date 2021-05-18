a53adb4c5763c78976119ecb5415c6b03828e583
Janu <janu.m.sung@gmail.com>
2021-05-17 17:15:54 -0700

    changed data and checked flow

--- src/Pages/CalculatorQuestion.jsx
+++ src/Pages/CalculatorQuestion.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../Components/Button";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 // data import
 import Questions from "../data/calcuatorQuestions.json";
@@ -9,24 +9,31 @@ import Questions from "../data/calcuatorQuestions.json";
 const CalculatorQuestion = () => {
   /** Grab the number that comes after /calculator in the url */
   const { number } = useParams();
-
+  const history = useHistory();
   /**
    * Filter through Questions file to find question to display
    * useParams returns a string, so we have to parseInt the `number` value
    * Filter returns an array so we grab the first element
    * and destructure the question properties: question, tooltip, and answers
    */
-  const { question, tooltip, answers } = Questions.filter(
+  const { question, tooltip, answers, next_question } = Questions.filter(
     (q) => q.id === parseInt(number)
   )[0];
+
+  const handleClick = (i) => {
+    // if yes --> extract next_question[0] if no--> extract next_question[1]
+    // once we have the index --> push it to the url
+    history.push(`/calculator/${next_question[i]}`);
+  };
 
   return (
     <div>
       <h1>{question}</h1>
       {tooltip}
       <ul>
-        {answers.map((a) => (
-          <Button text={a} onClick={() => console.log(a)} />
+        {answers.map((a, i) => (
+          // id the yes vs no button
+          <Button text={a} onClick={() => handleClick(i)} />
         ))}
       </ul>
     </div>
