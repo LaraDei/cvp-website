ba6664eda37e222e4ae2a41e13bb8ca90bffe498
Janu <janu.m.sung@gmail.com>
2021-05-17 17:19:51 -0700

    added notes on data

--- src/Pages/CalculatorQuestion.jsx
+++ src/Pages/CalculatorQuestion.jsx
@@ -5,6 +5,8 @@ import { useParams, useHistory } from "react-router-dom";
 // data import
 import Questions from "../data/calcuatorQuestions.json";
 
+//**! Thoughts on Data Structure: for the answers array --> we could also use tupules -->  "answers": [ ["yes", 0], ["no", 1],["text to display", 2]] --> or keep it as is --> reinforced data rules vs. nesting ??*/
+
 /** A page to display an individual question from the question list */
 const CalculatorQuestion = () => {
   /** Grab the number that comes after /calculator in the url */
@@ -21,8 +23,7 @@ const CalculatorQuestion = () => {
   )[0];
 
   const handleClick = (i) => {
-    // if yes --> extract next_question[0] if no--> extract next_question[1]
-    // once we have the index --> push it to the url
+    //grabbing index value at next_question[i] --> pushing to history --> history will trickle down to filter function on line 19 to update the question.
     history.push(`/calculator/${next_question[i]}`);
   };
 
@@ -32,7 +33,7 @@ const CalculatorQuestion = () => {
       {tooltip}
       <ul>
         {answers.map((a, i) => (
-          // id the yes vs no button
+          // adding index to handleClick function --> i will determine which index to grab from within next_question array --> which corresponds to the answer's index within answers array.
           <Button text={a} onClick={() => handleClick(i)} />
         ))}
       </ul>
